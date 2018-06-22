08e62a1935998ae028b14c0fdb7014f438cce5b2
jdubou <jordan.woo2@gmail.com>
2018-06-22 09:36:53 -0700

    Update jenkins.java

--- jenkins.java
+++ jenkins.java
@@ -1,1 +1,12 @@
-System.out.println("Jenkins");
+pipeline {
+    agent any
+    stages {
+stage('testing pipeline'){
+          steps{
+      echo 'test1'
+                sh 'mkdir from-jenkins'
+                sh 'touch from-jenkins/test.txt'
+                }
+        }
+}
+}
